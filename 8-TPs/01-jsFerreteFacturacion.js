84bf597ae2c445041f4ad63259f91de8c69ea4be
tomy1221 <tomasaguilar156@gmail.com>
2022-07-06 23:17:03 -0300

    3 tps hechos de e/s

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -1,4 +1,8 @@
-/*1.	Para el departamento de facturación:
+/*
+thomas aguilar arguello
+div x
+
+1.	Para el departamento de facturación:
 A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
 B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
 C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
